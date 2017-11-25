f7a5eb99fc27ec71080998919c8fb1e8d91f39df
emschimmel <emschimmel82@gmail.com>
2017-11-25 07:56:29 +0100

    less config

--- config.py
+++ config.py
@@ -1,19 +1,10 @@
 file_path           = '../img/'
 
 eye_pi_ip           = '127.0.0.1'
-eye_pi_port         = 30302
 face_pi_ip          = '127.0.0.1'
-face_pi_port        = 30303
 weather_pi_ip       = '127.0.0.1'
-weather_pi_port     = 30304
 music_pi_ip         = '127.0.0.1'
-music_pi_port       = 30305
 agenda_pi_ip        = '127.0.0.1'
-agenda_pi_port      = 30306
 kaku_pi_ip          = '127.0.0.1'
-kaku_pi_port        = 30307
-
 short_storage_ip    = '127.0.0.1'
-short_storage_port  = 30202
 long_storage_ip     = '127.0.0.1'
-long_storage_port   = 30203
