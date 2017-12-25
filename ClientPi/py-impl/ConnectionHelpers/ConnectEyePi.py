60a1e463922e64d201cf07f993481b616bc5ff60
emschimmel <emschimmel82@gmail.com>
2017-12-25 10:36:18 +0100

    less static

--- ClientPi/py-impl/ConnectionHelpers/ConnectEyePi.py
+++ ClientPi/py-impl/ConnectionHelpers/ConnectEyePi.py
@@ -13,13 +13,15 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
 from dns import resolver
+sys.path.append('../../')
+import config
 
 class ConnectEyePi:
 
     def resolve_eye_config(self):
         consul_resolver = resolver.Resolver()
-        consul_resolver.port = 8600
-        consul_resolver.nameservers = ["127.0.0.1"]
+        consul_resolver.port = config.consul_port
+        consul_resolver.nameservers = [config.consul_ip]
 
         dnsanswer = consul_resolver.query("eye-pi.service.consul.", 'A')
         ip = str(dnsanswer[0])
