c712045e1423bffbdcac6ed0793a639cbd0bc6d8
hansvanderpol <hans_vanderpol@icloud.com>
2017-12-29 20:18:37 +0100

    Get the eye pi service from consul

--- AdminConsolePi/py-impl/AdminConsoleApp.py
+++ AdminConsolePi/py-impl/AdminConsoleApp.py
@@ -2,18 +2,30 @@ from kivy.app import App
 
 from MainViewApp import MainView
 from LoginViewApp import LoginViewApp
+from AppConfig import AppConfig
+
 
 class AdminConsoleApp(App):
     def build(self):
         return MainView.build(self)
+
+    def build_config(self, config):
+        config.setdefaults('consul', {
+            'host': '127.0.0.1',
+            'port': '8600'
+        })
 
     def get_application_config(self):
         return super(AdminConsoleApp, self).get_application_config(
             'adminconsole.ini')
 
     def on_start(self, **kwargs):
+        ip, port = AppConfig().resolve_config(self.config.get('consul', 'host'), self.config.getint('consul', 'port'))
+
+        print(ip)
+        print(port)
+
         popup = LoginViewApp.build(self)
         popup.open()
-
 
 AdminConsoleApp().run()
---
+++ AdminConsolePi/py-impl/AppConfig.py
@@ -0,0 +1,16 @@
+import random
+
+from dns import resolver
+
+class AppConfig:
+    def resolve_config(self, host, port):
+        consul_resolver = resolver.Resolver()
+        print(consul_resolver)
+        consul_resolver.port = port
+        consul_resolver.nameservers = [host]
+
+        dnsanswer = consul_resolver.query("eye-pi.service.consul.", 'A')
+        ip = str(dnsanswer[0])
+        dnsanswer_srv = consul_resolver.query("eye-pi.service.consul.", 'SRV')
+        port = int(str(random.choice(dnsanswer_srv)).split()[2])
+        return ip, port
