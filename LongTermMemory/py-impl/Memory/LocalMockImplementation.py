717a6d082b3643480d80ac7e25a8cfbac0480f8c
emschimmel <emschimmel82@gmail.com>
2018-01-01 12:44:22 +0100

    local mock fix

--- LongTermMemory/py-impl/Memory/LocalMockImplementation.py
+++ LongTermMemory/py-impl/Memory/LocalMockImplementation.py
@@ -9,7 +9,9 @@ class LocalMockImplementation(object):
 
     def get(self, uniquename):
         print('local get %s' % self.__data)
-        return self.__data
+        for item in self.__mockList:
+            if item.uniquename == uniquename:
+                return item
 
     def get_all(self):
         print('local get')
@@ -21,12 +23,20 @@ class LocalMockImplementation(object):
 
     def store_new(self, value):
         print('local put')
+        self.__mockList.append(value)
 
     def update(self, uniquename, value, field):
         print('local update')
+        for item in self.__mockList:
+            if item.uniquename == uniquename:
+                item[field] = value
+
 
     def delete(self, uniquename):
         print('local delete')
+        for item in self.__mockList:
+            if item.uniquename == uniquename:
+                self.__mockList.remove(item)
 
     def __load_mock(self):
         return json.load(open('./Memory/personMock.json'))
